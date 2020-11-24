165ba8922a6f4f2201340b6ae9330f22c86aba2f
ishay320 <ishay320@users.noreply.github.com>
2020-11-25 00:50:24 +0200

    init

---
+++ myBank.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void bankCMD(){
+    int stop = 1;
+   while (stop)
+   {
+    char c;
+    printf("Transaction type?:\n");
+    scanf("%c",&c);
+    
+    switch (c)
+    {
+    case 'O':
+        printf("Initial deposit?:\n");
+        break;
+        case 'B':
+        printf("Account number?:\n");
+        break;
+        case 'D':
+        printf("Account number?:\n");
+        break;
+        case 'W':
+        printf("Account number?:\n");
+        break;
+        case 'C':
+        printf("Account number?:\n");
+        break;
+        case 'I':
+        printf("Interest rate?:\n");
+        break;
+        case 'P':
+        break;
+        case 'E':
+        stop = 0;
+        break;
+    default:
+        break;
+    }
+   }
+}
